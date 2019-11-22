dab9687af05beb21b269ad4521fdb96ed3c27f47
liron <lironpro14@gmail.com>
2019-11-22 16:29:36 +0200

    now bot returns okurigana

--- main.js
+++ main.js
@@ -13,8 +13,28 @@ kuroshiro.init(new KuromojiAnalyzer()).then(() => {
     const bot = new Telegraf(process.env.BOT_TOKEN);
 
     bot.on('message', (ctx) => {
-        const result = kuroshiro.convert(ctx.message.text, { to: "hiragana" });
+        const result = kuroshiro.convert(ctx.message.text, { to: "hiragana", mode: "okurigana" });
         result.then((actualResult) => ctx.reply(actualResult));
     });
     bot.launch();
+});
+
+// init project
+const express = require("express");
+const app = express();
+
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.send('Works!');
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
