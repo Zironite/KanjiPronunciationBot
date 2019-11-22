be3318df2f5b5d0d00529ba6d5ebf17eefa7dd61
liron <lironpro14@gmail.com>
2019-11-22 21:49:55 +0200

    now sending images also works

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
 const Kuroshiro = require("kuroshiro");
 const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
+const tesseract = require("tesseract.js");
+const request = require("request");
 
 const keyboard = Markup.inlineKeyboard([
     Markup.urlButton('❤️', 'http://telegraf.js.org'),
@@ -13,8 +15,34 @@ kuroshiro.init(new KuromojiAnalyzer()).then(() => {
     const bot = new Telegraf(process.env.BOT_TOKEN);
 
     bot.on('message', (ctx) => {
+      if(ctx.message.text) {
         const result = kuroshiro.convert(ctx.message.text, { to: "hiragana", mode: "okurigana" });
         result.then((actualResult) => ctx.reply(actualResult));
+      }
+      if(ctx.message.photo) {
+        let file = ctx.telegram.getFile(ctx.message.photo[2].file_id)
+        file.then((response) => {
+          let filePath = `https://api.telegram.org/file/bot${ctx.telegram.token}/${response.file_path}`;
+          request({
+            url: filePath,
+            method: "get",
+            encoding: null
+          }, (error,response,body) => {
+            if (error) {
+              console.error(error);
+            } else {
+              let job = tesseract.recognize(body, "jpn", { logger: m => console.log(m) });
+              job
+                .catch(err => console.error(err))
+                .then(result =>{
+                  kuroshiro.convert(result.data.text, { to: "hiragana", mode: "okurigana" })
+                    .then(actualResult => ctx.reply(actualResult));
+                })
+                .finally(resultOrError => console.log(resultOrError));
+            }
+          });
+        });
+      }
     });
     bot.launch();
 });
