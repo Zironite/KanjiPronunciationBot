7664445866810f3ff580b5c62f7c3f4e8642e3e6
liron <lironpro14@gmail.com>
2019-12-04 19:31:57 +0200

    code cleanup

--- main.js
+++ main.js
@@ -3,14 +3,7 @@ const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
 const Kuroshiro = require("kuroshiro");
 const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
-const tesseract = require("tesseract.js");
-const request = require("request");
-const sharp = require('sharp');
 
-const keyboard = Markup.inlineKeyboard([
-    Markup.urlButton('❤️', 'http://telegraf.js.org'),
-    Markup.callbackButton('Delete', 'delete')
-  ]);
 const kuroshiro = new Kuroshiro();
 kuroshiro.init(new KuromojiAnalyzer()).then(() => {
     const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -24,22 +17,15 @@ kuroshiro.init(new KuromojiAnalyzer()).then(() => {
     bot.launch();
 });
 
-// init project
 const express = require("express");
 const app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.send('Works!');
 });
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
