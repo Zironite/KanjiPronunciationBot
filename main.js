5ab50fd23090b6e1d5a4ec6563844f45a14d284b
liron <lironpro14@gmail.com>
2019-12-04 20:54:05 +0200

    now jisho is used to get meaning of kanji

--- main.js
+++ main.js
@@ -3,18 +3,51 @@ const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
 const Kuroshiro = require("kuroshiro");
 const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
+const kuromoji = require("kuromoji")
+const JishoApi = require("unofficial-jisho-api");
 
 const kuroshiro = new Kuroshiro();
-kuroshiro.init(new KuromojiAnalyzer()).then(() => {
+const kuromojiAnalyzer = new KuromojiAnalyzer();
+const jisho = new JishoApi();
+kuroshiro.init(kuromojiAnalyzer).then(() => {
+  kuromoji.builder({
+    dicPath: "./node_modules/kuromoji/dict/"
+  }).build((err, tokenizer) => {
     const bot = new Telegraf(process.env.BOT_TOKEN);
 
     bot.on('message', (ctx) => {
       if(ctx.message.text) {
         const result = kuroshiro.convert(ctx.message.text, { to: "hiragana", mode: "okurigana" });
-        result.then((actualResult) => ctx.reply(actualResult));
+        result.then((actualResult) => {
+          let translationPromises = [];
+          let parsedKanji = {};
+          tokenizer.tokenize(ctx.message.text).filter(element => Kuroshiro.Util.hasKanji(element.basic_form))
+            .map(element => {
+              for (let i = 0; i < element.basic_form.length; i++) {
+                let currChar = element.basic_form.charAt(i);
+
+                if (Kuroshiro.Util.hasKanji(currChar) && !parsedKanji.hasOwnProperty(currChar)) {
+                  parsedKanji[currChar] = true;
+                  translationPromises.push(jisho.searchForKanji(currChar).then((currKanjiResult) => {
+                    return {
+                      kanji: currChar,
+                      meaning: currKanjiResult.meaning
+                    };
+                  }));
+                }
+              }
+            });
+          Promise.all(translationPromises).then(values => {
+            let translationText = values.map(value => {
+              return `${value.kanji} => ${value.meaning}`
+            }).join("\n");
+            ctx.reply(`${actualResult}\n\n${translationText}`);
+          });
+        });
       }
     });
     bot.launch();
+  });
 });
 
 const express = require("express");
