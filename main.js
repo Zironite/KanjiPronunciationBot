80c4e1ea528a6ad10e5a64b8d9d4ff31d56ee576
liron <lironpro14@gmail.com>
2019-12-04 19:30:19 +0200

    no longer supporting photos

--- main.js
+++ main.js
@@ -20,37 +20,6 @@ kuroshiro.init(new KuromojiAnalyzer()).then(() => {
         const result = kuroshiro.convert(ctx.message.text, { to: "hiragana", mode: "okurigana" });
         result.then((actualResult) => ctx.reply(actualResult));
       }
-      if(ctx.message.photo) {
-        let file = ctx.telegram.getFile(ctx.message.photo[2].file_id)
-        file.then((response) => {
-          let filePath = `https://api.telegram.org/file/bot${ctx.telegram.token}/${response.file_path}`;
-          request({
-            url: filePath,
-            method: "get",
-            encoding: null
-          }, (error,response,body) => {
-            if (error) {
-              console.error(error);
-            } else {
-              sharp(body)
-                .resize(600,600, {
-                  fit: 'contain'
-                })
-                .toBuffer()
-                .then(data => {
-                  let job = tesseract.recognize(data, "jpn", { logger: m => console.log(m) });
-                  job
-                    .catch(err => console.error(err))
-                    .then(result =>{
-                      kuroshiro.convert(result.data.text, { to: "hiragana", mode: "okurigana" })
-                        .then(actualResult => ctx.reply(actualResult));
-                    })
-                    .finally(resultOrError => console.log(resultOrError));
-                });
-            }
-          });
-        });
-      }
     });
     bot.launch();
 });
