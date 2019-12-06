782c4ca09ccd94a3c4fe38a28aef472ebf0c79a2
liron <lironpro14@gmail.com>
2019-12-06 22:43:37 +0200

    now session is used for jisho
    this way, you can send just the command "/jisho".
    after that, the text you send next will be translated.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const Kuroshiro = require("kuroshiro");
 const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
 const kuromoji = require("kuromoji")
 const JishoApi = require("unofficial-jisho-api");
+const session = require('telegraf/session');
 
 const kuroshiro = new Kuroshiro();
 const kuromojiAnalyzer = new KuromojiAnalyzer();
@@ -15,41 +16,25 @@ kuroshiro.init(kuromojiAnalyzer).then(() => {
   }).build((err, tokenizer) => {
     const bot = new Telegraf(process.env.BOT_TOKEN);
 
+    bot.use(session())
     bot.command('jisho', (ctx) => {
-      let translationPromises = [];
-      let parsedKanji = {};
-      tokenizer.tokenize(ctx.message.text).filter(element => Kuroshiro.Util.hasKanji(element.basic_form))
-        .map(element => {
-          for (let i = 0; i < element.basic_form.length; i++) {
-            let currChar = element.basic_form.charAt(i);
-
-            if (Kuroshiro.Util.hasKanji(currChar) && !parsedKanji.hasOwnProperty(currChar)) {
-              parsedKanji[currChar] = true;
-              translationPromises.push(jisho.searchForKanji(currChar).then((currKanjiResult) => {
-                return {
-                  kanji: currChar,
-                  meaning: currKanjiResult.meaning
-                };
-              }));
-            }
-          }
-        });
-      Promise.all(translationPromises).then(values => {
-        let translationText = values.map(value => {
-          return `${value.kanji} => ${value.meaning}`
-        }).join("\n");
-        if (translationText !== "")
-          ctx.reply(translationText);
-        else
-          ctx.reply("Please input text to translate")
-      });
+      if (ctx.message.text === "/jisho") {
+        ctx.reply("What word would you like to learn about?");
+        ctx.session.use_jisho = true;
+      } else {
+        jishoTranslation(ctx, tokenizer);
+      }
     });
     bot.on('message', (ctx) => {
       if(ctx.message.text) {
-        const result = kuroshiro.convert(ctx.message.text, { to: "hiragana", mode: "okurigana" });
-        result.then((actualResult) => {
-          ctx.reply(actualResult);
-        });
+        if (ctx.session.use_jisho) {
+          jishoTranslation(ctx, tokenizer);
+        } else {
+          const result = kuroshiro.convert(ctx.message.text, { to: "hiragana", mode: "okurigana" });
+          result.then((actualResult) => {
+            ctx.reply(actualResult);
+          });
+        }
       }
     });
     bot.launch();
@@ -68,3 +53,34 @@ app.get("/", function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+function jishoTranslation(ctx,tokenizer) {
+  let translationPromises = [];
+  let parsedKanji = {};
+  tokenizer.tokenize(ctx.message.text).filter(element => Kuroshiro.Util.hasKanji(element.basic_form))
+    .map(element => {
+      for (let i = 0; i < element.basic_form.length; i++) {
+        let currChar = element.basic_form.charAt(i);
+
+        if (Kuroshiro.Util.hasKanji(currChar) && !parsedKanji.hasOwnProperty(currChar)) {
+          parsedKanji[currChar] = true;
+          translationPromises.push(jisho.searchForKanji(currChar).then((currKanjiResult) => {
+            return {
+              kanji: currChar,
+              meaning: currKanjiResult.meaning
+            };
+          }));
+        }
+      }
+    });
+  Promise.all(translationPromises).then(values => {
+    let translationText = values.map(value => {
+      return `${value.kanji} => ${value.meaning}`
+    }).join("\n");
+    if (translationText !== "")
+      ctx.reply(translationText);
+    else
+      ctx.reply("Please input text to translate")
+    ctx.session.use_jisho = false;
+  });
+}
